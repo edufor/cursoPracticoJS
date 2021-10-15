a25b6cdc8c964c41337cfeb04cccabb29d0da42f
edufor <eduardo.forero.r@gmail.com>
2021-10-15 08:20:45 -0500

    Se creó el boilerplate para el nuevo ejercicio sobre Descuentos con JS

--- figuras.js
+++ figuras.js
@@ -95,3 +95,20 @@ function areaCirculo(radio) {
 // console.log("El área del circulo es " + areaCirculo + " cm^2");
 
 console.groupEnd();
+
+function calcularPerimetroCuadrado() {
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+
+function calcularAreaCuadrado() {
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+
+    const area = areaCuadrado(value);
+    alert(area);
+
+}
